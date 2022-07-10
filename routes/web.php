64b019e654ab6bbdc59c04a1c4806db509eb091b
gerrygeop <gerryja28@gmail.com>
2022-07-11 06:19:52 +0800

    update routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\Dapur\DapurController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Dapur\{DapurController, CategoryController, MenuController, ReservationController, TableController};
 
 Route::get('/', function () {
     return view('welcome');
@@ -13,6 +13,11 @@ Route::get('/dashboard', function () {
 
 Route::middleware(['auth', 'admin'])->name('dapur.')->prefix('dapur')->group(function () {
     Route::get('/', [DapurController::class, 'index'])->name('index');
+
+    Route::resource('/categories', CategoryController::class);
+    Route::resource('/menus', MenuController::class);
+    Route::resource('/tables', TableController::class);
+    Route::resource('/reservations', ReservationController::class);
 });
 
 require __DIR__ . '/auth.php';
