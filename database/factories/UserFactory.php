74d92ee6d61da247a58cc06c4afffd4f11f72d22
gerrygeop <gerryja28@gmail.com>
2022-07-11 04:49:24 +0800

    add laravel breeze

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,10 +18,11 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->safeEmail(),
+            'name' => 'Superadmin',
+            'email' => 'superadmin@gmail.com',
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'is_admin' => true,
             'remember_token' => Str::random(10),
         ];
     }
