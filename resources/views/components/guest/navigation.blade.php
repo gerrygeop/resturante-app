08222f1e7b0820219d8789d745e38bc64fe96bfb
gerrygeop <gerryja28@gmail.com>
2022-07-17 21:59:23 +0800

    update UI guest welcome page

---
+++ resources/views/components/guest/navigation.blade.php
@@ -0,0 +1,46 @@
+<div class="bg-white shadow-md" x-data="{ isOpen: false }">
+   <nav class="container px-6 py-8 mx-auto md:flex md:justify-between md:items-center">
+      <div class="flex items-center justify-between">
+         <a class="text-xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 md:text-2xl hover:text-green-400"
+            href="#">
+            TailFood
+         </a>
+         <!-- Mobile menu button -->
+         <div @click="isOpen = !isOpen" class="flex md:hidden">
+            <button type="button" class="text-gray-800 hover:text-gray-400 focus:outline-none focus:text-gray-400"
+               aria-label="toggle menu">
+               <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
+                  <path fill-rule="evenodd"
+                     d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z">
+                  </path>
+               </svg>
+            </button>
+         </div>
+      </div>
+
+      <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
+      <div :class="isOpen ? 'flex' : 'hidden'"
+         class="flex-col mt-8 space-y-4 md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0">
+         <a class="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 hover:text-green-400"
+            href="/">
+            Home
+         </a>
+         <a class="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 hover:text-green-400"
+            href="/#about">
+            About Us
+         </a>
+         <a class="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 hover:text-green-400"
+            href="{{ route('guest.categories.index') }}">
+            Categories
+         </a>
+         <a class="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 hover:text-green-400"
+            href="{{ route('guest.menus.index') }}">
+            Our Menu
+         </a>
+         <a class="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 hover:text-green-400"
+            href="{{ route('guest.reservations.step.one') }}">
+            Make Reservation
+         </a>
+      </div>
+   </nav>
+</div>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,21 +1,11 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+@extends('layouts.master')
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+@section('content')
+   <x-guest.navigation />
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+   <div class="min-h-screen">
+      {{ $slot }}
+   </div>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body>
-        <div class="font-sans text-gray-900 antialiased">
-            {{ $slot }}
-        </div>
-    </body>
-</html>
+   <x-guest.footer />
+@endsection
